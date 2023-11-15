0fcf94a893a927d6bc914d8da8f24540fb402f2d
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2023-11-14 23:48:36 -0500

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+           <p className='text-[#487FC6] font-bold p-2'>Virtual Experience Research Group</p>
+           <h1 className='md:text-7xl sm:text-6x; text-4xl font-bold md:py-6'>Creating a Patient</h1> 
+
+           <div>
+           </div>
+           <div>
+            <button className='bg-[#487FC6] w-[200px] rounded-md font-medium my-6 mx-autp py-3 '>Get Started</button>
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home;
