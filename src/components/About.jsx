ba14cd0f64d09315137d9d4db921f068a0782cb5
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2023-11-19 23:47:49 -0500

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../images/healthLogo.png';
+import { Link } from 'react-router-dom';
 
-export const About = () => {
+const About = () => {
   return (
-    <div>About</div>
-  )
+    <div className='w-full bg-white py-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        <img className='w-[500px] mx-auto my-4' src={Logo} alt='/' />
+        <div className='flex flex-col justify-center'>
+          <p className='text-[#487FC6] text-center font-bold'>Virtual Experience Research Group</p>
+          <h1 className='text-[#12266C] text-center md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
+            About Us
+          </h1>
+          <p>
+            We are a passionate team dedicated to creating virtual human patients for research and education.
+            Our mission is to provide a seamless virtual experience for healthcare professionals and students.
+          </p>
+          <button className='bg-[#487FC6] text-[#FFFFFF] w-[200px] rounded-md font-medium my-6 mx-auto py-3'>
+            <Link to='/Patients'>Learn More</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default About;
