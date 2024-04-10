b9345b1e6c2e874351aa8102d135f556b3c24181
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2024-04-10 11:16:41 -0400

    adjust main ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import DoctorImage from '../images/doctor.png';
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-white flex justify-center items-center'>
+    <div className='w-full h-screen bg-white flex justify-center items-center mt-40'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 gap-8 items-center mt-[-550px]'> 
         <div className='flex flex-col items-center justify-center'>
           <h1 className='text-7xl font-bold mb-8 text-center'>Welcome to PatientPortal</h1>
@@ -14,7 +14,7 @@ const Home = () => {
             create a virtual patient agent within a click of a button. Learn more by watching the video
             or simply getting started to start your own virtual human journey!
           </p>
-          <Link to='/Patients' className='bg-[#353D53] text-white w-80 h-16 rounded-md font-bold text-xl flex justify-center items-center mt-4'> 
+          <Link to='/Patients' className='bg-[#353D53] text-white w-80 h-16 rounded-md font-bold text-xl flex justify-center items-center mt-2'> 
             Create your own patient!
           </Link>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
   };
 
   return (
-    <div className='fixed left-0 top-0 w-[250px] bg-[#353D53] text-white flex flex-col justify-between h-full'>
+    <div className='fixed left-0 top-0 w-[160px] bg-[#353D53] text-white flex flex-col justify-between h-full'>
       <div className='flex flex-col items-center'>
-        <div className='mb-10 text-3xl font-bold p-4 items-center'>PatientPortal</div>
+        <div className='mb-10 text-2xl font-bold p-4 items-center'>PatientPortal</div>
         <ul className='flex flex-col items-center'>
           <li className='mb-10 p-4 font-bold flex flex-col items-center'>
             <Link to='/Patients'><SlPeople size={50} /> </Link>
@@ -29,9 +29,6 @@ const Navbar = () => {
             <Link to='/About'>About</Link>
           </li>
         </ul>
-      </div>
-      <div className='p-4' onClick={handleNav}>
-        {nav ? <AiOutlineClose size={40} /> : <AiOutlineMenu size={20} />}
       </div>
     </div>
   );
