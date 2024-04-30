00afe83c2c7136d4677c66079777d41e7fdc47d3
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2024-04-30 19:26:20 -0400

    homepage update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import Logo from '../images/healthLogo.png'; 
-import DoctorImage from '../images/doctor.png'; 
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import Demo from '../images/laurademo.gif';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNav = () => {
+    const uniqueId = uuidv4();
+    localStorage.setItem('uniqueId', uniqueId);
+    axios.post('http://localhost:3001/api/login', {userId: uniqueId})
+      .then(response => {
+        const { visitNum } = response.data;
+        navigate(`/Chat/${uniqueId}`, { state: { visitNum } });
+      })
+      .catch(error => {
+        console.error('Error checking visit', error);
+      });
+  }
+
   return (
-    <div className='w-full h-screen bg-white flex justify-center items-center'>
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 gap-8 items-center mt-[-550px]'> 
-        <div className='flex flex-col items-center justify-center'>
-          <h1 className='text-7xl font-bold mb-8 text-center'>Welcome to PatientPortal</h1>
-          <p className='text-2xl mb-8 text-center'> 
-            Welcome to the Virtual Experience Virtual Patient Creator. This website is designed to
-            create a virtual patient agent within a click of a button. Learn more by watching the video
-            or simply getting started to start your own virtual human journey!
-          </p>
-          <Link to='/Patients' className='bg-[#353D53] text-white w-80 h-16 rounded-md font-bold text-xl flex justify-center items-center mt-4'> 
-            Create your own patient!
-          </Link>
-        </div>
-        <div className='flex justify-center'>
-          <img src={DoctorImage} alt='Doctor' className='shadow-md' />
-        </div>
+    <div className='flex h-screen bg-gray-50'>
+      <div className='flex-1 flex flex-col justify-center items-start pl-16 pr-32'>
+        <h1 className='text-8xl font-bold mb-10'>Welcome to PatientPortal</h1>
+        <p className='mb-10 text-2xl'>
+          Welcome to the Virtual Experience Virtual Patient Creator. This website is designed to 
+          create a virtual patient agent within a click of a button. Learn more by watching the 
+          video or simply getting started to start your own virtual human journey!
+        </p>
+        <button
+          onClick={handleNav} 
+          className='text-blue-600 flex items-center w-max py-2 px-6 border border-blue-600 hover:bg-blue-600 hover:text-white transition-colors rounded-full font-medium text-lg'
+        >
+          Talk to Laura &rarr;
+        </button>
+      </div>
+      <div className='flex-1 flex justify-center items-center pr-16'>
+        <img src={Demo} alt='Patient' className='shadow-lg max-w-full h-auto' />
       </div>
     </div>
   );
